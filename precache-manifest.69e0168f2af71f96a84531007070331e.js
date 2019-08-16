12c81a8a0d3364c827806f689bbc0c7ecb45614e
Programmer900 <ionizator231@yandex.ru>
2019-08-16 15:15:06 +0700

    first commit

---
+++ precache-manifest.69e0168f2af71f96a84531007070331e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "03c8696c1dddb05f79c6",
+    "url": "js/chunk-vendors.7ffe2a2d.js"
+  },
+  {
+    "revision": "c76ed29956a0f0512de6",
+    "url": "js/chunk-73ed5ebc.4ad44a29.js"
+  },
+  {
+    "revision": "1fb61c4273bfb7c1f7bb",
+    "url": "js/chunk-637d95be.89fc98fc.js"
+  },
+  {
+    "revision": "9b41e4a002cf45f70c0f",
+    "url": "js/app.913b8b67.js"
+  },
+  {
+    "revision": "7a3757815b8d9d239365b9f19d403f94",
+    "url": "index.html"
+  },
+  {
+    "revision": "03c8696c1dddb05f79c6",
+    "url": "css/chunk-vendors.d07f484d.css"
+  },
+  {
+    "revision": "1fb61c4273bfb7c1f7bb",
+    "url": "css/chunk-637d95be.380fd734.css"
+  },
+  {
+    "revision": "9b41e4a002cf45f70c0f",
+    "url": "css/app.f451b32f.css"
+  }
+];
